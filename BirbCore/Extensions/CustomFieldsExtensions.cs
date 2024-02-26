b42b1a9fce69bb7ba67b1016dfd56c7a4ff76f32
Drbirbdev <drbirbdev@gmail.com>
2024-02-25 20:44:49 -0800

    Add TryGetInt extension for CustomFields

---
+++ BirbCore/Extensions/CustomFieldsExtensions.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace BirbCore.Extensions;
+public static class CustomFieldsExtensions
+{
+    public static int? TryGetInt(this Dictionary<string, string>? customFields, string key)
+    {
+        if (customFields == null)
+        {
+            return null;
+        }
+        if (!customFields.TryGetValue(key, out string value))
+        {
+            return null;
+        }
+        if (!int.TryParse(value, out int result))
+        {
+            return null;
+        }
+        return result;
+    }
+}
